a8399ac0be7fb0f567e54f18e4ca5ec6c34ec60e
sorenmacharis <r0806867@student.thomasmore.be>
2022-03-04 11:09:46 +0100

    push van 04-03
    authenticatie werkt met memberstatus

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,45 +2,52 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.contrib.auth.decorators import login_required, permission_required
 
 from .models import Question, Choice
+
 # Create your views here.
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-    def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
-
-def vote(request, question_id):
+@login_required(login_url='/accounts/login/')
+def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
+    return render(request, 'polls/detail.html', {'question': question})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    return render(request, 'polls/profile.html')
+
+  
+@permission_required('polls.view_question', raise_exception=True)       
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+    
+@login_required(login_url='/accounts/login/')
+def vote(request, question_id):
+    
+        question = get_object_or_404(Question, pk=question_id)
+        try:
+            selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
         
-        return render(request, 'polls/detail.html', {
+            return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
+            })
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
         
-        return render(request, 'polls/detail.html',{
-            'votesuccess': "success",
-        })
-    return HttpResponse("You're voting on question %s." % question_id)
+            return render(request, 'polls/detail.html',{
+                'votesuccess': "success",
+            })
+    
 
 def numberofpolls(request):
     nq = Question.objects.count()
