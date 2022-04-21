302d27855153c50a9a5ea505e7075469168edf7b
sorenmacharis <r0806867@student.thomasmore.be>
2022-04-21 08:38:24 +0200

    push 21-04

--- mysite/polls/components/vote.py
+++ mysite/polls/components/vote.py
@@ -4,26 +4,26 @@ from django.shortcuts import render, get_object_or_404
 from polls.models import Question, Choice
 
 class VoteView(UnicornView):
-    which : str = 0
+    which : str = ''
     error_message : str = ''
-    votesuccess : str = None
+    votesuccess : bool = False
     q : Question = None
 
     def mount(self):
         self.q = get_object_or_404(Question, pk=self.request.GET.get('question_id',''))
-        
-    
+
+
     def addVote(self):
         try:
             selected_choice = self.q.choice_set.get(pk=self.which)
         except (KeyError, Choice.DoesNotExist):
-        
+
             self.error_message = "You didn't select a choice!"
         else:
             selected_choice.votes += 1
             selected_choice.save()
-        
-            self.votesuccess = 'success'
+
+            self.votesuccess = True
 
     class Meta:
         # Zonder dit zijn er bepaalde problemen met de votesuccess & radiobuttons
